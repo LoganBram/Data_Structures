408c77e1f898b440f27b9e2b651212036f059930
Logan Bramwell <loganbramwellqu@gmail.com>
2023-04-09 19:36:53 -0400

    saving before implementing search for part 2, adding within search2 function. everything else works

--- a3graphs_hash.py/WordFreqHash.py
+++ a3graphs_hash.py/WordFreqHash.py
@@ -7,12 +7,13 @@ import random
 class ClosedHashtable:
 
     def __init__(self):
-        self.hashsize = 100
+        self.hashsize = 51
         self.table = [None]*self.hashsize
         self.stop_words = set(stopwords.words('english'))
         self.words = []
 
-        self.p2_hashsize = 51
+        # max of 50 words in the part 2 section, hence size of 51
+        self.p2_hashsize = 50
         self.p2_table = [None]*self.p2_hashsize
 
     def fileloader(self, filepath):
@@ -57,90 +58,70 @@ class ClosedHashtable:
     def add(self, words):
         for word in words:
             slot = self.hashfunc(word)
+            start = slot
             insert = False
-            counter = slot
+            while insert == False:
 
-            # while the word has not been inserted
-            while insert != True and counter < self.hashsize:
-                # if slot is empty, insert word
-                if self.table[counter] == None:
-                    self.table[counter] = [word, 1]
+                if self.table[slot] is None:
+                    self.table[slot] = [word, 1]
                     insert = True
                     break
-                # if word is same as word in slot, increment freq
-                if self.table[counter][0] == word:
-                    thing = self.table[counter]
-                    thing[1] += 1
-                    self.table[counter] = thing
+                elif word == self.table[slot][0]:
+                    self.table[slot][1] += 1
                     insert = True
                     break
-                # if counter + 1 = original has slot, it means loop of entire array completed, and table is full
-                if counter + 1 == slot:
-                    print("table is still full")
+                slot = (slot+1) % self.hashsize
+                if slot == start:
                     insert = True
                     break
-                # if counter == hashsize - 1, it means we have reached the end of the array, so we need to wrap around
-                # negative one because we increment counter at the end of the loop
-                if counter == self.hashsize - 1:
-                    counter = -1
-                # Linear probe
-                else:
-                    counter += 1
 
-        "---part2---"
+        "---part2------------------------------------------------------"
+
+    def p2_hashfunc(self, word):
+        total = 0
+
+        for i in range(len(word)):
+            total = total + (i+1) * ord(word[i])
+
+        total = total % self.p2_hashsize
+        return total
 
     def p2_add(self, words):
         for word in words:
-            slot = self.hashfunc(word)
+            slot = self.p2_hashfunc(word)
+            start = slot
             insert = False
-            counter = slot
-
-            # while the word has not been inserted
-            while insert != True and counter < self.p2_hashsize:
-                # if slot is empty, insert word
-                if self.p2_table[counter] == None:
-                    self.p2_table[counter] = [word, 1]
+            while insert == False:
+                if self.p2_table[slot] is None:
+                    self.p2_table[slot] = [word, 1]
                     insert = True
                     break
-                # if word is same as word in slot, increment freq
-                if self.p2_table[counter][0] == word:
-                    thing = self.p2_table[counter]
-                    thing[1] += 1
-                    self.p2_table[counter] = thing
+                elif word == self.p2_table[slot][0]:
+                    self.p2_table[slot][1] += 1
                     insert = True
                     break
-                # if counter + 1 = original has slot, it means loop of entire array completed, and table is full
-                if counter + 1 == slot:
-                    print("table is still full")
+                slot = (slot+1) % self.p2_hashsize
+                if slot == start:
                     insert = True
                     break
-                # if counter == hashsize - 1, it means we have reached the end of the array, so we need to wrap around
-                # negative one because we increment counter at the end of the loop
-                if counter == self.p2_hashsize - 1:
-                    counter = -1
-                # Linear probe
 
-                counter += 1
-                print(self.p2_table)
-
-    def search(self):
+    def p2_search(self):
         print(self.p2_table)
         for k in range(5, 25, 5):
+            print("k =", k*2)
+            print("")
             list = []
             for i in range(k):
                 x = random.randint(0, len(self.p2_table)-1)
-
                 list.append(self.p2_table[x][0])
-
                 x = "notinlist"
                 list.append(x)
-
             for i in list:
                 steps = 0
                 for j in range(len(self.p2_table)):
                     steps += 1
                     try:
-                        if steps == len(self.p2_table)-1:
+                        if steps == len(self.p2_table)+1:
                             print(i, "is not in the table")
                             break
                         if i == self.p2_table[j][0]:
@@ -154,20 +135,17 @@ obje = ClosedHashtable()
 
 
 obje.fileloader('D:/A3test.txt')
-obje.add(obje.words)
-obje.p2_add(obje.words)
-obje.search()
-
-
-obje.search()
+# obje.add(obje.words)
+# obje.p2_add(obje.words)
+# obje.p2_search()
 
 
 class Node:
 
-    def __init__(self, data):
+    def __init__(self, data, freq):
         self.data = data
         self.next = None
-        self.freq = 1
+        self.freq = freq
 
 
 class OpenHashtable:
@@ -177,8 +155,8 @@ class OpenHashtable:
         self.hashtable = []
         self.stop_words = set(stopwords.words('english'))
         self.words = []
-
-        self.p2_hashsize = 51
+        # size of 50 for part 2 due to max hashtable size being 50
+        self.p2_hashsize = 50
         self.p2_hashtable = []
 
     def fileloader(self, filepath):
@@ -249,6 +227,7 @@ class OpenHashtable:
                 curr = curr.next
 
     # prints the hashtable
+
     def print(self):
         for i in range(self.hashsize):
             print("index", i)
@@ -265,11 +244,50 @@ class OpenHashtable:
 
     """ -------------------- PART 2 ------------------------------------------------------------"""
 
-    def print_2(self):
+    def construct2(self):
+        for i in range(self.p2_hashsize):
+            add = Node('Head', 0)
+            self.p2_hashtable.append([add])
+            # print(self.hashtable[i][0].data)
+
+    def add2(self):
+        for word in self.words:
+            new_node = Node(word, 1)
+            # get correct slot
+            slot = self.hashfunc2(word)
+            # set current to the first node in the slot
+            curr = self.p2_hashtable[slot][0]
+            done = False
+            # if the first node is empty, add the new node
+            while done == False:
+                if curr.next == None and curr.data != new_node.data:
+                    curr.next = new_node
+                    done = True
+                # add to word frequency if the word is already in the hashtable
+                elif curr.data == new_node.data:
+                    curr.freq += 1
+                    done = True
+                # traverse
+                curr = curr.next
+
+    def search2(self):
+        """"""
+        for k in range(5, 25, 5):
+            print("k =", k*2)
+            print("")
+            list = []
+            for i in range(k):
+                x = random.choice(self.words)
+                list.append(x)
+                x = "notinlist"
+                list.append(x)
+            print(list)
+
+    def print2(self):
         for i in range(self.p2_hashsize):
             print("index", i)
             # set current to the first node in the slot
-            curr = self.hashtable[i][0]
+            curr = self.p2_hashtable[i][0]
             # traverse
             list = []
             while curr != None:
@@ -279,6 +297,16 @@ class OpenHashtable:
                 curr = curr.next
             print(list)
 
+    def hashfunc2(self, word):
+        total = 0
+
+        for i in range(len(word)):
+            total = total + (i+1) * ord(word[i])
+
+        total = total % self.p2_hashsize
+        print(total)
+        return total
+
     def main(self):
         obj.fileloader('D:/A3test.txt')
         obj.construct()
@@ -286,14 +314,16 @@ class OpenHashtable:
         obj.print()
 
     def main2(self):
-        pass
+        obj2.fileloader('D:/A3test.txt')
+        obj2.construct2()
+        obj2.add2()
+        # obj2.print2()
+        obj2.search2()
 
 
-""" -------------------- PART 2 ------------------------------------------------------------"""
-
-
-# obj = OpenHashtable()
+obj = OpenHashtable()
 # obj.main()
-
+obj2 = OpenHashtable()
+obj2.main2()
 
 # for part 2) make size 51 because the most that will be in the hashtable is 50, due to K words being at most 50 in our case
