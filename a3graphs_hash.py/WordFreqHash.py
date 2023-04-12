b7aea3e48254a305abe1e355732618d685b4bba8
Logan Bramwell <loganbramwellqu@gmail.com>
2023-04-12 16:21:34 -0400

    fixed searching starting from beginning, print statements to clarify

--- a3graphs_hash.py/WordFreqHash.py
+++ a3graphs_hash.py/WordFreqHash.py
@@ -17,6 +17,7 @@ class ClosedHashtable:
         self.words = []
 
         # max of 50 words being searched for in the part 2 section due to k <= 50, hence hashtablesize of 50
+        # Yes, it doesnt include all the words within the textfile, but it only includes the exact amount words necessary
 
         self.p2_hashsize = 50
         self.p2_table = [None]*self.p2_hashsize
@@ -79,7 +80,7 @@ class ClosedHashtable:
                 if slot == start:
                     insert = True
                     break
-        print("OPEN HASH TABLE p1 of size: ", self.hashsize)
+        print("CLOSED HASH TABLE p1 of size: ", self.hashsize)
         print("")
         print(self.table)
         print("")
@@ -112,44 +113,54 @@ class ClosedHashtable:
                 if slot == start:
                     insert = True
                     break
-        print("---------------OPEN HASH TABLE p2 of size: ", self.p2_hashsize)
+        print("---------------CLOSED HASH TABLE p2 of size: ", self.p2_hashsize)
         print("")
-
-    def p2_search(self):
         print(self.p2_table)
-        for k in range(5, 25, 5):
+
+    def p2_searchtest(self):
+        # searching function for closded hash table
+        # starts at step = 1, because im counting going to the hashing index as a step
+        for k in range(5, 30, 5):
             print("")
             print("k =", k*2)
             print("")
+            # list of words we will look for
             list = []
+            # add k*2 words to list since in my loop k is half of the actual k
             for i in range(k):
+                # allows me to add one word in the table and one word not in the table
                 x = random.randint(0, len(self.p2_table)-1)
                 list.append(self.p2_table[x][0])
                 x = "notinlist"+str(i)
                 list.append(x)
-            for i in list:
-                steps = 0
-                for j in range(len(self.p2_table)):
+            # iterate through words in our list to find
+            for word in list:
+                slot = self.p2_hashfunc(word)
+                print("searching for: ", word, ", starting at index: ", slot)
+                start = slot
+                found = False
+                steps = 1
+                while found == False:
+                    if word == self.p2_table[slot][0]:
+                        found = True
+                        print(word, "is in the table, after", steps, "steps")
+                        print("")
+                        break
+                    slot = (slot+1) % self.p2_hashsize
                     steps += 1
-                    try:
-                        if steps == len(self.p2_table):
-                            print(i, "is not in the table -1")
-                            break
-                        if i == self.p2_table[j][0]:
-                            print(i, "is in the table, steps= ", steps)
-                            break
-                    except:
-                        pass
+                    # if done full loop and not found, then not in table
+                    if slot == start:
+                        found = True
+                        print(word, "is not in the table -1")
+                        print("")
+                        break
 
 
 obje = ClosedHashtable()
-
-
 obje.fileloader('D:/A3test.txt')
 obje.add(obje.words)
-
 obje.p2_add(obje.words)
-obje.p2_search()
+obje.p2_searchtest()
 
 
 "-----OPEN HASH TABLE----------------"
@@ -166,13 +177,18 @@ class Node:
 class OpenHashtable:
 
     def __init__(self,):
-        self.hashsize = 5
+        # NOTE: while coding i changed some things to cater towards pt2, hence the seperate functions for p1 and p2
+        # but im realizing now alot of it wasnt necessary, regardless it still follows the instructions correctly
+        # just harder to read
+        self.hashsize = 13
         self.hashtable = []
         self.stop_words = set(stopwords.words('english'))
         self.words = []
-        # size of 50 for part 2 due to max hashtable size being 50
-        self.p2_hashsize = 50
+
+        # max amount of values being stored is 50, so having around 4 per bucket on avg seems optimum to avoid any values having their own bucket
+        self.p2_hashsize = 13
         self.p2_hashtable = []
+        self.p2_words = []
 
     def fileloader(self, filepath):
         self.root = None
@@ -205,8 +221,8 @@ class OpenHashtable:
         return " ".join([word for word in str(text).split() if word not in self.stop_words])
 
     def hashfunc(self, word):
+        # using same hash function through entirety of code as introduced by the slides in class
         total = 0
-
         for i in range(len(word)):
             total = total + (i+1) * ord(word[i])
 
@@ -258,16 +274,21 @@ class OpenHashtable:
                 curr = curr.next
             print(list)
 
-    """ -------------------- PART 2 ------------------------------------------------------------"""
+    """ -------------------- PART 2 for open ------------------------------------------------------------"""
 
     def construct2(self):
         for i in range(self.p2_hashsize):
             add = Node('Head', 0)
             self.p2_hashtable.append([add])
-            # print(self.hashtable[i][0].data)
 
     def add2(self):
-        for word in self.words:
+
+        # gets 50 random words from the list of all the words in the text file
+        for i in range(50):
+            x = random.randint(0, len(self.words)-1)
+            self.p2_words.append(self.words[x])
+        # iterates through the chosen words and adds them to the hashtable
+        for word in self.p2_words:
             new_node = Node(word, 1)
             # get correct slot
             slot = self.hashfunc2(word)
@@ -290,40 +311,43 @@ class OpenHashtable:
     def search2(self):
 
         for k in range(5, 25, 5):
+            print(" going to index counts as a step, lowest steps possible as per my definition is 3 due to head node")
             print("k =", k*2)
             print("")
             list = []
+            # adds words to list following assignment instructions
             for i in range(k):
-                x = random.choice(self.words)
+                x = random.choice(self.p2_words)
                 list.append(x)
                 x = "notinlist"
                 list.append(x)
-            print(list)
+                # iterates through each word we are looking for and sends it to be searched
             for i in list:
                 self.searchhelper2(i)
 
     # takes word from search list as input and searches for it in the hashtable
     def searchhelper2(self, word):
-        steps = 0
+        # start steps at 1 because im counting going to correct bucket as a step
+        # an item cannot have less then 3 steps due to first step being going to correct bucket, and first item in bucket being head placeholder
+        # therefore requiring 3 steps to get to the first possible node of any of the buckets
+        steps = 1
         found = False
-        for j in range(self.p2_hashsize):
-            steps += 1
-            # print("index", i)
-            # set current to the first node in the slot
-            curr = self.p2_hashtable[j][0]
-            if found == True:
+        slot = self.hashfunc2(word)
+        curr = self.p2_hashtable[slot][0]
+        while curr != None:
+            if curr.data == word:
+                steps += 1
+                print("found", word, "in", steps, "steps")
+                print("")
+                found = True
                 break
             else:
-                while curr != None:
-                    steps += 1
-                    if curr.data == word:
-                        print("found", word, "in", steps, "steps")
-                        found = True
-                        break
-                    else:
-                        curr = curr.next
-
-            # traverse
+                steps += 1
+                curr = curr.next
+                if curr == None:
+                    print("not found", word, "-1")
+                    print("")
+                    break
 
     def hashfunc2(self, word):
         total = 0
@@ -337,6 +361,9 @@ class OpenHashtable:
 
     # prints out the hashtable
     def print2(self):
+        print("")
+        print("----------OPEN Hashtable for part 2------")
+        print(" going to index counts as a step, lowest steps possible as per my definition is 3 due to head node")
         for i in range(self.p2_hashsize):
             print("")
             print("index", i)
@@ -348,9 +375,8 @@ class OpenHashtable:
             while curr != None:
                 x = curr.data, curr.freq
                 list.append(x)
-                # print(curr.data, curr.freq)
-                curr = curr.next
 
+                curr = curr.next
             print(list)
 
     def main(self):
@@ -373,5 +399,3 @@ obj.main()
 
 obj2 = OpenHashtable()
 obj2.main2()
-
-# for part 2) make size 51 because the most that will be in the hashtable is 50, due to K words being at most 50 in our case
